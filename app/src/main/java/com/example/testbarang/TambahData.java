64d684fab47204f96020d08e7951041244cc05b5
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-05-13 12:20:30 +0700

    Retrieve references to the firebase database

--- app/src/main/java/com/example/testbarang/TambahData.java
+++ app/src/main/java/com/example/testbarang/TambahData.java
@@ -3,12 +3,27 @@ package com.example.testbarang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class TambahData extends AppCompatActivity {
+    private DatabaseReference database;
+    private Button btnSubmit;
+    private EditText etKode, etNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_data);
+
+        etKode = findViewById(R.id.editNo);
+        etNama = findViewById(R.id.editNama);
+        btnSubmit = findViewById(R.id.btnOK);
+
+        //mengambil referensi ke firebase database
+        database = FirebaseDatabase.getInstance().getReference();
     }
 }
