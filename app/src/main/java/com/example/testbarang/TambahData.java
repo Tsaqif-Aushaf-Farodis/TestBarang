17c3b6f765c638866f753896599125269176b55a
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-05-13 12:43:45 +0700

    Send data to the firebase RealTime Database

--- app/src/main/java/com/example/testbarang/TambahData.java
+++ app/src/main/java/com/example/testbarang/TambahData.java
@@ -2,7 +2,9 @@ package com.example.testbarang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethod;
@@ -11,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -43,6 +46,20 @@ public class TambahData extends AppCompatActivity {
                 InputMethodManager imm = (InputMethodManager)
                         getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(etKode.getWindowToken(), 0);
+            }
+        });
+    }
+
+    public void submitBrg(Barang brg){
+        /*Berikut ini adalah kode yang digunakan untuk mengirimkan data
+        * ke firebase RealTime Database dan juga kita set onSuccessListener
+        * yang berisi kode yang akan dijalankan ketika data berhasil ditambahkan*/
+        database.child("Barang").push().setValue(brg).addOnSuccessListener(this, new OnSuccessListener<Void>(){
+            @Override
+            public void onSuccess(Void aVoid){
+                etKode.setText("");
+                etNama.setText("");
+                Toast.makeText(getApplicationContext(), "Data berhasil ditambahkan", Toast.LENGTH_LONG).show();
             }
         });
     }
