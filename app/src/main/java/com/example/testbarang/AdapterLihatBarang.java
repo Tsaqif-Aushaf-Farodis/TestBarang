e99f68077b487b5c16a1d9f53103a4afcb16137e
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-06-09 08:43:24 +0700

    Create class AdapterLihatBarang & class ViewHolder

---
+++ app/src/main/java/com/example/testbarang/AdapterLihatBarang.java
@@ -0,0 +1,36 @@
+package com.example.testbarang;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AdapterLihatBarang extends RecyclerView.Adapter<AdapterLihatBarang.ViewHolder> {
+    @NonNull
+    @Override
+    public AdapterLihatBarang.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterLihatBarang.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
